c0b02a2978837cac1a3cc0b01b054fa4e05ed479
ArnavK <professionalcoder1337@gmail.com>
2025-12-03 18:01:10 +0530

    code cleanup and minor frontend changes

--- ind/app.py
+++ ind/app.py
@@ -1,20 +1,15 @@
 from flask import Flask , redirect , render_template , request, url_for
 from dotenv import load_dotenv
 import os
-
 import pickle, bz2
-import pandas as pd
 import numpy as np
-
 import requests
 from requests.structures import CaseInsensitiveDict
-
 import warnings
 warnings.filterwarnings("ignore")
 
 #Environment variables for API keys
 load_dotenv()
-
 
 # Import Classification and Regression model file for Algeria
 C_pickle = bz2.BZ2File(r'static\alg\Classification.pkl', 'rb')
@@ -22,25 +17,33 @@ alg_model_C = pickle.load(C_pickle)
 R_pickle = bz2.BZ2File(r'static\alg\Regression.pkl', 'rb')
 alg_model_R = pickle.load(R_pickle)
 
+#Importing model files for India
+file_path_lr = "static/ind/finalModel_lr.sv"
+file_path_dt = "static/ind/finalModel_dt.sv"
+file_path_rf = "static/ind/finalModel_rf.sv"
+
+ind_model_lr = pickle.load(open(file_path_lr , 'rb'))
+ind_model_dt = pickle.load(open(file_path_dt , 'rb'))
+ind_model_rf = pickle.load(open(file_path_rf , 'rb'))
 
 app = Flask(__name__)
 
-
-
+#home page
 @app.route('/')
 def home():
 	return render_template("indexMain.html")
 
-
+#page about explaining terms
 @app.route('/learn-more')
 def learn_more():
 	return render_template("learn.html")
 
+#showing model accuracy and stuff
 @app.route('/statistics')
 def stats():
 	return render_template("stats.html")
 
-
+#comparison with previous works in domain
 @app.route('/statistics/compare')
 def compare():
 	sections = [
@@ -103,44 +106,39 @@ def compare():
 
 	return render_template("comp.html", sections=sections)
 
-
-
+"""Indian forest fires"""
+#model selection page for indian fires
 @app.route('/ind/model-select')
 def ind_home():
 	return render_template("ind/ind_model_sel.html")
 
-
+#for India prediction using Logistic Regression Model
 @app.route('/ind/predict-fire-lr' , methods = ['GET' , 'POST'])
 def ind_predict_lr():
 	msg = ""
+	#user has submitted form
 	if request.method == 'POST':
 		try:
 			temp = np.float64(request.form['Temperature']) / 100
 			oxygen = np.float64(request.form['Oxygen']) / 100
 			humid = np.float64(request.form['Humidity']) / 100
 
-			folder_path = 'static/ind'
-			file_name = 'finalModel_lr.sv'
-
-			file_path = os.path.join(folder_path , file_name)
-
-			predictor = pickle.load(open(file_path , 'rb'))
-			prediction = predictor.predict(np.array([[oxygen , temp , humid]]))[0]
+			prediction = ind_model_lr.predict(np.array([[oxygen , temp , humid]]))[0]
 
 			if(prediction > 0):
 				msg = "Fire can occur under these conditions."
 			else:
 				msg = "No Worry! Fire will not take place."
-
 			return render_template("ind/predict_page_lr.html" , prediction_fire = msg, indback="Back")
-
+		#in case of error in input validation
 		except:
 			return render_template("ind/predict_page_lr.html" , prediction_fire = "Check the input again!!!", indback="Back")
 
+	#user has only loaded the page, not submitted the form
 	else:
 		return render_template("ind/predict_page_lr.html" , prediction_fire = msg, indback="Back")
 
-
+#for India prediction using Decision Tree Model
 @app.route('/ind/predict-fire-dt' , methods = ['GET' , 'POST'])
 def ind_predict_dt():
 	msg = ""
@@ -150,13 +148,7 @@ def ind_predict_dt():
 			oxygen = np.float64(request.form['Oxygen']) / 100
 			humid = np.float64(request.form['Humidity']) / 100
 
-			folder_path = 'static/ind'
-			file_name = 'finalModel_dt.sv'
-
-			file_path = os.path.join(folder_path , file_name)
-
-			predictor = pickle.load(open(file_path , 'rb'))
-			prediction = predictor.predict(np.array([[oxygen , temp , humid]]))[0]
+			prediction = ind_model_dt.predict(np.array([[oxygen , temp , humid]]))[0]
 
 			if(prediction > 0):
 				msg = "Fire can occur under these conditions."
@@ -169,7 +161,7 @@ def ind_predict_dt():
 	else:
 		return render_template("ind/predict_page_dt.html" , prediction_fire = msg, indback="Back")
 
-
+#for India prediction using Random Forest Model
 @app.route('/ind/predict-fire-rf' , methods = ['GET' , 'POST'])
 def ind_predict_rf():
 	msg = ""
@@ -179,13 +171,7 @@ def ind_predict_rf():
 			oxygen = np.float64(request.form['Oxygen']) / 100
 			humid = np.float64(request.form['Humidity']) / 100
 
-			folder_path = 'static/ind'
-			file_name = 'finalModel_rf.sv'
-
-			file_path = os.path.join(folder_path , file_name)
-
-			predictor = pickle.load(open(file_path , 'rb'))
-			prediction = predictor.predict(np.array([[oxygen , temp , humid]]))[0]
+			prediction = ind_model_rf.predict(np.array([[oxygen , temp , humid]]))[0]
 
 			if(prediction > 0):
 				msg = "Fire can occur under these conditions."
@@ -198,7 +184,7 @@ def ind_predict_rf():
 	else:
 		return render_template("ind/predict_page_rf.html" , prediction_fire = msg, indback="Back")
 
-
+#for India prediction for multiple points using user selected Model
 @app.route('/ind/predict-multi' , methods = ['GET' , 'POST'])
 def ind_predict_multi():
 	msg = ""
@@ -208,8 +194,7 @@ def ind_predict_multi():
 			model=request.form['mdname']
 			points = vals.split("\r\n")
 			res=[]
-			# print(lst1)
-			# temp = np.float64(request.form['Temperature']) / 100
+			#mapping numbers to model name for later using in form
 			if model == '1':
 				mod='Logistic Regression'
 			elif model == '2':
@@ -217,13 +202,14 @@ def ind_predict_multi():
 			elif model == '3':
 				mod='Random Forest'
 
+			#iterating over the points in the input and assigning prediction scores to each of them
 			for i in range (0,len(points)):
 				t,o,h = points[i].split(",")
-				points[i]=f"Point {i+1} (Temp: {t}, Oxy: {o}, Hum: {h})"
+				points[i]=f"Point {i+1} (Temp: {t}, Oxy: {o}, Hum: {h})"	#changes list element for better readability in frontend
 				t = np.float64(t) / 100
 				o = np.float64(o) / 100
 				h = np.float64(h) / 100
-
+				#predicting for each point
 				pred = ind_multi(t,o, h, model)
 
 				if(pred > 0):
@@ -232,7 +218,6 @@ def ind_predict_multi():
 					msg = "No"
 
 				res.append(msg)
-			
 			return render_template("ind/multires.html" , indback="Back", pts=points, res =res, md = mod)
 			
 		except:
@@ -240,44 +225,34 @@ def ind_predict_multi():
 	else:
 		return render_template("ind/multi.html" , prediction_fire = msg, indback="Back")
 
-
-
+#used when multiple points
 def ind_multi(tp,ox, hum, mod):
-	folder_path = 'static/ind'
 	if mod=="1":
-		file_name = 'finalModel_lr.sv'
+		prediction = ind_model_lr.predict(np.array([[ox , tp , hum]]))[0]
 	elif mod=="2":
-		file_name = 'finalModel_dt.sv'
+		prediction = ind_model_dt.predict(np.array([[ox , tp , hum]]))[0]
 	elif mod=="3":
-		file_name = 'finalModel_rf.sv'
-
-	file_path = os.path.join(folder_path , file_name)
-
-	predictor = pickle.load(open(file_path , 'rb'))
-	prediction = predictor.predict(np.array([[ox , tp , hum]]))[0]
+		prediction = ind_model_rf.predict(np.array([[ox , tp , hum]]))[0]
 
 	return prediction
 
-
+#page for selecting city, based on which current prediction will be given
 @app.route('/ind/api/select-city' , methods = ['GET' , 'POST'])
 def ind_api_sel():
 	return render_template("ind/city_sel.html",indback="Back")
 
-
+#route for displaying prediction based on current data
 @app.route('/ind/api/result' , methods = ['GET' , 'POST'])
 def ind_api_res():
 	msg = ""
 	if request.method == 'POST':
 		try:
+			#in case of names entered by user in both text box and dropdown selected, entered tet takes priority
 			city= request.form["city_name_text"]
 			if not city:
 				city= request.form["city_name"]
-				if not city:
-					# return render_template("ind/city_sel.html" , prediction_fire = "Select city!!!")
-					# return redirect(url_for('city_page', prediction_fire="Select city!!!"))
-					print("huh")
+				if not city:	#means user did not enter any city or select one, so return them to the page
 					return redirect(url_for('ind_api_sel'))
-
 
 			model=request.form['mdname']
 			if model == '1':
@@ -286,21 +261,14 @@ def ind_api_res():
 				mod='Decision Tree'
 			elif model == '3':
 				mod='Random Forest'
-			try:
-				lat, lon, e  = city_api(city)
-				# print(e)
-				# return render_template("ind/city_sel.html" , prediction_fire = e)			
-				# return redirect(url_for('ind_api_sel'))
 
-			except Exception as e:
-				print(e)
+			try:
+				lat, lon  = city_api(city)
+			except Exception as e:	#incase of any error in fetching data from api
+				# print(e)
 				return redirect(url_for('ind_api_sel'))
-				# return render_template("ind/city_sel.html" , prediction_fire = e)
 
 			t,o,h = weather_api(lat, lon)
-
-			# pred_res = ind_multi(t,o,h, model)
-
 			pred = ind_multi(t,o, h, model)
 
 			if(pred > 0):
@@ -315,62 +283,48 @@ def ind_api_res():
 					Current Temperature: {t}℃<br>
 					Current Humidity: {h}%<br>
 					Predicted fire: {msg}<br>
-					bruh; {pred}
 					"""
-
 			return render_template("ind/api_res.html", api_res=1, content=cont, model=mod,indback="Back")
 		
 		except Exception as e:
-			# return render_template("ind/city_sel.html" , prediction_fire = "Check the input again!!!")
-			print(e)
 			return redirect(url_for('ind_api_sel'))
-
-
-
 	else:
-		# return render_template("ind/city_sel.html")
 		return redirect(url_for('ind_api_sel'))
 
-
-	# code= request.form["city_code"]
-	
-
-	# use ind multi here and resutns
-
-
+#for fetching coordinates (latitude & longitude) based on city name entered
 def city_api(c):
-		# url = "https://api.geoapify.com/v1/geocode/search?text=38%20Upper%20Montagu%20Street%2C%20Westminster%20W1H%201LJ%2C%20United%20Kingdom&apiKey={apik}"
 		try:
 			apik=os.environ.get("CITY_API")
 			lst = c.split(" ")
-			er=''
+			#optimising entered city name to work with url, in case of citties with space in their name
+			#if splitted list has more than one element => city has space in name
+			#so, for searching with API, 'New Delhi' has to be made into 'New%20Delhi'
 			if len(lst)>1:
 				s = ""
 				for i in range (0,len(lst)):
 					s += lst[i]
 					if i!=(len(lst)-1):
 						s+= "%20"
-
 				url = f"https://api.geoapify.com/v1/geocode/search?text={s}&apiKey={apik}"
 
 			else:				
 				url = f"https://api.geoapify.com/v1/geocode/search?text={c}&apiKey={apik}"
 
+			#procedure from API documentation
 			headers = CaseInsensitiveDict()
 			headers["Accept"] = "application/json"
-
 			resp = requests.get(url, headers=headers)
 			js = resp.json()
+
 			longitude = js['features'][0]['properties']['lon']
 			latitude = js['features'][0]['properties']['lat']
-
-			return latitude, longitude, er
+			return latitude, longitude
 		
-		except Exception as e:
-			return 0,0, e
+		except Exception:
+			return 0,0
 		# print(resp.status_code)
 
-
+#for fetching weather conditions based on latitude and longitude
 def weather_api(lat,lon):
 	apik = os.environ.get("WEATHER_API")
 	url=f"http://api.weatherapi.com/v1/current.json?key={apik}&q={lat},{lon}&aqi=no"
@@ -378,17 +332,19 @@ def weather_api(lat,lon):
 	g = resp.json() 
 	temp = g['current']['temp_c']
 	humid = g['current']['humidity'] 
-	# oxy = 49.25
+	#no reliable source for Oxygen concentration found
 	oxy = 15
-
 	return temp, oxy, humid
 
 
+"""Algerian Forest fires"""
+
+#model selection page for Algeria
 @app.route('/alg/model-select')
 def alg_home():
 	return render_template("alg/alg_model_sel.html")
 
-
+#for predicting Alregia fires with Classification Model
 @app.route('/alg/predict-fire-class', methods=['GET', 'POST'])
 def alg_predict_class():
 	if request.method == 'POST':
@@ -405,23 +361,19 @@ def alg_predict_class():
 			Float_features = [float(x) for x in features]
 			final_features = [np.array(Float_features)]
 			prediction = alg_model_C.predict(final_features)[0]
-			# prediction= float(pred)
-			# log.info('Prediction done for Classification model')
 
 			if prediction == 0:
 				text = 'Forest is Safe!'
 			else:
 				text = 'Forest is in Danger!'
 			return render_template('alg/alg_class.html', prediction_text1="{} --- Chance of Fire is {:.4f}".format(text, prediction), algback="Back")
+			
 		except Exception as e:
-            # log.error('Input error, check input', e)
-			# print('Input error, check input', e)
-			# return render_template('alg/alg_class.html', prediction_text1=e)
 			return render_template('alg/alg_class.html', prediction_text1="Check the Input again!!!", algback="Back")
 	else:
 		return render_template('alg/alg_class.html', algback="Back")
 
-
+#for predicting Algeria fires with Regression Model
 @app.route('/alg/predict-fire-reg', methods=['GET', 'POST'])
 def alg_predict_reg():
 	if request.method == 'POST':
@@ -439,24 +391,18 @@ def alg_predict_reg():
 			final_features = [np.array(Float_features)]
 			prediction = alg_model_R.predict(final_features)[0]
 
-
-			# log.info('Prediction done for Regression model')
-
 			if prediction > 15:
 				return render_template('alg/alg_reg.html', prediction_text2="Fuel Moisture Code index is {:.4f} ---- Warning!!! High hazard rating".format(prediction), algback="Back")
 			else:
 				return render_template('alg/alg_reg.html', prediction_text2="Fuel Moisture Code index is {:.4f} ---- Safe.. Low hazard rating".format(prediction), algback="Back")
 
 		except Exception as e:
-			# log.error('Input error, check input', e)
-			# print('Input error, check input', e)
 			return render_template('alg/alg_reg.html', prediction_text2="Check the Input again!!!", algback="Back")
             
 	else:
 		return render_template('alg/alg_reg.html', algback="Back")
 		
-
-
+#for predicting Algeria fires for multiple points using mdoelselected by user
 @app.route('/alg/predict-multi' , methods = ['GET' , 'POST'])
 def alg_predict_multi():
 	if request.method == 'POST':
@@ -464,7 +410,6 @@ def alg_predict_multi():
 			vals=request.form['multi']
 			model=request.form['mdname']
 			points = vals.split("\r\n")
-			# res=[]
 			tex=[]
 
 			if model == '1':
@@ -483,47 +428,19 @@ def alg_predict_multi():
 				pred = alg_multi(t,w,f,d,i,model)
 
 				tex.append(pred)
-				# res.append(chn)
-
 			return render_template("alg/multires.html" , algback="Back", pts=points, tex=tex, md = mod)
 
-
-
-
 		except Exception as e:
-            # log.error('Input error, check input', e)
-			# print('Input error, check input', e)
-			# return render_template('alg/alg_class.html', prediction_text1=e)
-			# return render_template('alg/multi.html', prediction_fire=e, algback="Back")
 			return render_template('alg/multi.html', prediction_fire="Check the Input again!!!", algback="Back")
 	else:
 		return render_template('alg/multi.html', algback="Back")
 
-
+#used for predicting for multiple points in Algeria
 def alg_multi(t,w,f,d,i, md):
 	features = [t, w,f, d, i]
 
 	Float_features = [float(x) for x in features]
 	final_features = [np.array(Float_features)]
-
-
-	# if md=='1':
-	# 	chance = alg_model_C.predict(final_features)[0]
-	# 	if chance == 0:
-	# 		text = 'Safe!'
-	# 	else:
-	# 		text = 'Danger'
-	# 	# return render_template('alg/alg_class.html', prediction_text1="{} --- Chance of Fire is {:.4f}".format(text, prediction))
-
-
-	# elif md=='2':
-	# 	chance = alg_model_R.predict(final_features)[0]
-	# 	if chance > 15:
-	# 		# return render_template('alg/alg_reg.html', prediction_text2="Fuel Moisture Code index is {:.4f} ---- Warning!!! High hazard rating".format(prediction))
-	# 		text = 'Danger!'
-	# 	else:
-	# 		# return render_template('alg/alg_reg.html', prediction_text2="Fuel Moisture Code index is {:.4f} ---- Safe.. Low hazard rating".format(prediction))
-	# 		text = 'Safe'
 
 	if md=='1':
 		chance = alg_model_C.predict(final_features)[0]
@@ -534,53 +451,7 @@ def alg_multi(t,w,f,d,i, md):
 		text = 'Safe'
 	else:
 		text = 'Danger!'
-		
-	# ch = "{:.2f}".format(chance)
-
 	return text
-	# prediction= float(pred)
-	# log.info('Prediction done for Classification model')
-
 
 if __name__ == '__main__':
 	app.run(debug = True)
-
-
-
-
-# @app.route('/create-model-ind' , methods = ['GET' , 'POST'])
-# def ind_create_Model():
-# 	score = ""
-# 	if request.method == 'POST':
-# 		algo = request.form.get('Model_Selection')
-# 		file = pd.read_csv("static/ind/ForestFireDataSetUpdated2.1.csv")
-# 		score = round((ind_modelScale(file , algo) * 100) , 2)
-# 		return render_template('ind/model.html' , accuracy_model = 'Accuracy of the Selected Model - ' + str(score - 2) + '%')
-	
-# 	return render_template('ind/model.html' , accuracy_model = 'Select Model')
-
-
-# def ind_modelScale(data:pd.DataFrame , model_name:str):
-# 	features_list = ['Oxygen', 'Temperature', 'Humidity']
-# 	for i in features_list:
-# 		data[i] = MinMaxScaler().fit_transform(data[[i]])
-# 	X = data.drop(columns= ["Area" , "Fire Occurrence"] , axis = 1)
-# 	Y = data["Fire Occurrence"]
-# 	X_train , X_test , Y_train , Y_test = train_test_split(X , Y , random_state = 100 , test_size = 0.3)
-# 	global modelClassifier
-# 	if model_name == "DTC":
-# 		modelClassifier = DecisionTreeClassifier(min_samples_leaf = 2 , ccp_alpha = 0.48 , random_state = 0)
-# 	elif model_name == "RFC":
-# 		modelClassifier = RandomForestClassifier(min_samples_leaf = 2 , ccp_alpha = 0.21 , random_state = 0)
-# 	else:
-# 		modelClassifier = LogisticRegression()
-# 	modelClassifier.fit(X_train , Y_train)
-
-# 	folder_path = 'static'
-# 	file_name = 'finalModel.sv'
-
-# 	file_path = os.path.join(folder_path , file_name)
-
-# 	pickle.dump(modelClassifier , open(file_path , 'wb'))
-# 	return accuracy_score(Y_test , modelClassifier.predict(X_test))
-
